24baa8b4e55ed816baafdb6705de358a8d3472c1
Seb Vince <sebvince@amd.com>
2025-11-10 11:36:23 -0600

    Ref version for custom scheduling 256x160x64 NN

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -522,31 +522,60 @@ def hasCustomSchedule(kernel):
     elif is256x160x64DTL and is16bit and not isMixed and ([GRVWA, GRVWB, LRVW] == [8,8,8]) and MI == [16,16,32,1] and MIWG == [2,2]:
         kernel["MfmaInitCVgprs"] = True
 
-        optSchedule = dict()
-        syncCode = []
-        if isTN and TLDS == 1:
+        if isTN and not useLDSTr:
+            optSchedule = dict()
+            syncCode = []
+            if isTN and TLDS == 1:
+                optSchedule = {
+                    'SYNC'   : [[-1,8,21, 21,63, 63]],
+                    #Addr. update (be done before GRA/GRB)
+                    'GRIncB' : [[0,1,2,3,4,5,6,7,8]],
+                    'GRIncA' : [[9,10,11,12,13,14,15,16,17]],
+                    #Current iteration
+                    'LRA0'   : [[0,1,2,3,4,5,6,7]],
+                    'LRB0'   : [[8,9,10,11,12]],
+                    #Buffer loads.
+                    'GRA'    : [[19,21, 23,23, 26,26, 30,30, 33,33, 37,37, 40,40, 44,44]],
+                    'GRB'    : [[47,47, 51,51, 54,54, 57,57, 61,61]],
+                    #Prefetch next iteration.
+                    'LRA1'   : [[64,66,67,68,69,70,71,72]],
+                    'LRB1'   : [[65,73,74,75,76]],
+                    'LRSA'   : [[39]],#swap buffers
+                    'LRSB'   : [[39]],#swap buffers
+                    'LWSA'   : [[61]],#swap buffers
+                    'LWSB'   : [[61]],#swap buffers
+                    'LCC'   : [[79, 79]], #loop control
+                }
+                syncCode = [SWaitCnt(dscnt=4, vlcnt=-1, vscnt=-1, comment=""),
+                            SWaitCnt(dscnt=8, vlcnt=-1, vscnt=-1, comment=""),
+                            SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment=""),
+                            SBarrier(comment=""),
+                            SWaitCnt(dscnt=-1, vlcnt=(5 + 8), vscnt=-1, comment="Wait for previous GRA to completely"),
+                            SBarrier(comment="")]
+        elif isNN and useLDSTr and TLDS==1:
+
             optSchedule = {
-                'SYNC'   : [[-1,8,21, 21,63, 63]],
+                'SYNC'   : [[-1,8,20, 20,63, 63]],
                 #Addr. update (be done before GRA/GRB)
                 'GRIncB' : [[0,1,2,3,4,5,6,7,8]],
                 'GRIncA' : [[9,10,11,12,13,14,15,16,17]],
                 #Current iteration
-                'LRA0'   : [[0,1,2,3,4,5,6,7]],
-                'LRB0'   : [[8,9,10,11,12]],
+                'LRA0'   : [[1,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10]],
+                'LRB0'   : [[2,11,12,13,14]],
                 #Buffer loads.
-                'GRA'    : [[19,21, 23,23, 26,26, 30,30, 33,33, 37,37, 40,40, 44,44]],
+                'GRA'    : [[20,20, 23,23, 26,26, 30,30, 33,33, 37,37, 40,40, 44,44]],
                 'GRB'    : [[47,47, 51,51, 54,54, 57,57, 61,61]],
                 #Prefetch next iteration.
-                'LRA1'   : [[64,66,67,68,69,70,71,72]],
-                'LRB1'   : [[65,73,74,75,76]],
-                'LRSA'   : [[39]],#swap buffers
-                'LRSB'   : [[39]],#swap buffers
-                'LWSA'   : [[61]],#swap buffers
-                'LWSB'   : [[61]],#swap buffers
+                'LRA1'   : [[64,65,65,66,66,67,67,68,68,69,69,70,70,71,71,72]],
+                'LRB1'   : [[64,73,74,75,76]],
+                'LRSA'   : [[39]],
+                'LRSB'   : [[39]],
+                'LWSA'   : [[61]],
+                'LWSB'   : [[61]],
                 'LCC'   : [[79, 79]], #loop control
             }
             syncCode = [SWaitCnt(dscnt=4, vlcnt=-1, vscnt=-1, comment=""),
-                        SWaitCnt(dscnt=8, vlcnt=-1, vscnt=-1, comment=""),
+                        SWaitCnt(dscnt=14, vlcnt=-1, vscnt=-1, comment=""),
                         SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment=""),
                         SBarrier(comment=""),
                         SWaitCnt(dscnt=-1, vlcnt=(5 + 8), vscnt=-1, comment="Wait for previous GRA to completely"),
