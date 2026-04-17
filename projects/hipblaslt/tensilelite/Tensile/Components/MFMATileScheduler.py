9a54d7d60c4c059a33748743fcdf91feaa327337
Seb Vince <sebvince@amd.com>
2026-04-17 08:39:46 -0500

    add wairlr_sync on all LR->GR dep

--- projects/hipblaslt/tensilelite/Tensile/Components/MFMATileScheduler.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/MFMATileScheduler.py
@@ -1196,9 +1196,7 @@ class MFMATileScheduler:
                 for gr in slot.grs:
                     same, cross = self._split_deps(gr.deps, pi, gr.subIterK_slot)
                     gr.deps = same
-                    gr.preOps = []
-                    if cross:
-                        gr.preOps.append(DepOp(kind='wait_lr_sync'))
+                    gr.preOps = [DepOp(kind='wait_lr_sync')]
 
         self._completed.add('remove_deps')
 
