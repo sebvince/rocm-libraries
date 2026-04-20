8d0e1a4967ec728b932ceb2402e86edd1654d593
Seb Vince <sebvince@amd.com>
2026-04-20 04:06:24 -0500

    Fix bug on gr_emit code

--- projects/hipblaslt/tensilelite/Tensile/Components/InstructionEmitter.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/InstructionEmitter.py
@@ -179,11 +179,11 @@ class InstructionEmitter:
         tensor = source.tensor
         tc = {'A': 'A', 'B': 'B', 'SA': 'MXSA', 'SB': 'MXSB'}.get(tensor, tensor)
         module = Module()
-        module.add(globalReadPtrUpdates(tc, self.writer, self.kernel))
-        module.add(globalReadLDSBufferSwap(tc, self.writer, self.kernel))
         if tensor in ('SA', 'SB'):
             module.add(globalReadScalePtrUpdates(tc, self.writer, self.kernel))
-            module.add(globalReadLDSBufferSwap(tc, self.writer, self.kernel))
+        else:
+            module.add(globalReadPtrUpdates(tc, self.writer, self.kernel))
+        module.add(globalReadLDSBufferSwap(tc, self.writer, self.kernel))
         return list(module.flatitems())
 
     def emit_gr_scale(self, source):
