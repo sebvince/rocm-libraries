5e247475adff62f2d034e51c138e4b047fa50c96
Seb Vince <sebvince@amd.com>
2026-04-27 09:20:09 -0500

    Draft PGR1

--- projects/hipblaslt/tensilelite/Tensile/Components/Subtile/Kernel.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/Subtile/Kernel.py
@@ -1122,7 +1122,7 @@ def preLoop(writer, kernel):
 def mainLoop(writer, kernel):
   module = Module()
   pgr = kernel["PrefetchGlobalRead"]
-  assert pgr in (0, 2), "SubtileBasedKernel only supports PGR=0 and PGR=2, got PGR=%d" % pgr
+  assert pgr in (0, 1, 2), "SubtileBasedKernel only supports PGR=0, PGR=1, and PGR=2, got PGR=%d" % pgr
 
   from Tensile.Components.SubtileBasedLogicalScheduler import (
       LogicalScheduler, SchedulerConfig as MFMASchedulerConfig,
