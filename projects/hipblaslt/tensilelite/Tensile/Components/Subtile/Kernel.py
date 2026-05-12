052a7a8e0f27f95fc5f04689e469d677f4280094
Seb Vince <sebvince@amd.com>
2026-05-12 08:32:34 -0500

    Remove logs and exception handler

--- projects/hipblaslt/tensilelite/Tensile/Components/Subtile/Kernel.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/Subtile/Kernel.py
@@ -1168,17 +1168,14 @@ def mainLoop(writer, kernel):
           partitionSizeN=partSizeN,
           pgr=schedulerPgr
       )
-      try:
-        scheduler = LogicalScheduler(cfg)
-        scheduler.build()
+      
+      scheduler = LogicalScheduler(cfg)
+      scheduler.build()
 
-        print(f"  Trying partition: M={cfg.partitionSizesM}, N={cfg.partitionSizesN} ({cfg.numPartitions} partitions)")
-        numVgpr = scheduler.getNumVgpr(tiA, tiB, scaleTiA, scaleTiB)
-        print(f"  partition: M={cfg.partitionSizesM}, N={cfg.partitionSizesN} ({cfg.numPartitions} partitions, {numVgpr} vgprs)")
-        if vgprUsed + numVgpr <= vgprBudget:
-            break
-      except Exception:
-          continue
+      numVgpr = scheduler.getNumVgpr(tiA, tiB, scaleTiA, scaleTiB)
+      if vgprUsed + numVgpr <= vgprBudget:
+          break
+      
       
   
   scheduler.allocVgprTiles(writer, tiA, tiB,
